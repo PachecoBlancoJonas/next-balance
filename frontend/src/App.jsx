fcd52807d53e68951e5a27136aad69e37817b5ce
Jonas <Pacheco.Blanco.Jonas@gmail.com>
2025-02-27 23:07:57 +0100

    change ignore

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import "./App.css";
 import { BanksComponent } from "./components/BanksComponent";
 import { BankLinkComponent } from "./components/BankLinkComponent";
 
 function App() {
-  const [transacciones, setTransacciones] = React.useState(
-    "Transacciones iniciales"
-  );
-  function cargarTransacciones() {
-    setTransacciones("Nuevas transacciones");
-  }
-  return (
-    <>
-      <BanksComponent />
-      <BankLinkComponent />
-    </>
-  );
+    const [transacciones, setTransacciones] = React.useState(
+        "Transacciones iniciales"
+    );
+    function cargarTransacciones() {
+        setTransacciones("Nuevas transacciones");
+    }
+    return (
+        <>
+            <BanksComponent />
+            <BankLinkComponent />
+        </>
+    );
 }
 
 export default App;
